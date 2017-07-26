ac084224716a58bf21d1e32ac3cfeb33321ba21b
硫酸 <lsch001@163.com>
2017-07-26 17:14:27 +0800

    Update
    mocha

---
+++ test/test-creation.js
@@ -0,0 +1,44 @@
+const helpers = require('yeoman-test');
+const assert = require('yeoman-assert');
+const fs = require('fs');
+const fsextra = require('fs-extra');
+const path = require('path');
+
+const basedir = path.join(__dirname, '../generators/app');
+
+describe('yo phaser-h5', () => {
+
+    let tmpdir;
+
+    beforeEach(() => {
+        return helpers.run(basedir)
+            .inTmpDir(dir => {
+                tmpdir = dir;
+            })
+            .withOptions({ projectName: 'temp' })
+    });
+
+    afterEach(() => fsextra.remove(tmpdir));
+
+    it('creates expected files', () => {
+
+        const expected = [
+            // add files you expect to exist here.
+            'gulpfile.js',
+            '.jshintrc',
+            'src/assets/css/css.css',
+            'index.html',
+            'package.json',
+            'src/assets/js/main.js',
+            'src/assets/js/entities/Gesture.js',
+            'src/assets/js/entities/MovieClip.js',
+            'src/assets/js/entities/VideoConfig.js',
+            'src/assets/js/states/boot.js',
+            'src/assets/js/states/state1.js',
+            'src/assets/js/states/preloader.js'
+        ];
+
+        const files = expected.map(i => path.join(tmpdir, i));
+        assert.file(files);
+    });
+});
