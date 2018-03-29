0ef270191f080402b9fff2a8de6b3a0544a72d02
硫酸 <lsch001@163.com>
2018-03-29 13:30:04 +0800

    :sparkles: Add load progress

--- generators/app/templates/src/assets/js/states/preloader.js
+++ generators/app/templates/src/assets/js/states/preloader.js
@@ -248,13 +248,22 @@
             var that = this;
             that.pgGraphics.clear();
             that.pgGraphics.lineStyle(6, 0x29ABE2);
-            that.pgGraphics.arc(this.game.width / 2, 182 + 140, 45, this.game.math.degToRad(270), this.game.math.degToRad(360 * _progress / 100 + 270), false);
+            that.pgGraphics.arc(this.game.width / 2, this.game.height / 2, 45, this.game.math.degToRad(270), this.game.math.degToRad(360 * _progress / 100 + 270), false);
             that.pgGraphics.endFill();
 
         },
         create: function() {
             var that = this;
             that.pgGraphics = this.add.graphics(0, 0);
+            var style = {
+                fontStyle: 'italic',
+                font: '"Helvetica Neue",Helvetica,Arial,"Microsoft Yahei","Hiragino Sans GB","Heiti SC","WenQuanYi Micro Hei"',
+                fontSize: 19,
+                fill: '#000',
+                align: 'center'
+            };
+            that.progress = this.add.text(this.game.width / 2, this.game.height / 2 + 5, '0%', style);
+            that.progress.anchor.setTo(0.5);
             this.load.onFileComplete.add(that.onfileComplete, this);
             this.load.onLoadComplete.addOnce(that.onLoadComplete, this);
             this.loadResources();
@@ -269,6 +278,7 @@
         onfileComplete: function(progress, cacheKey, success, totalLoaded, totalFiles) {
             var that = this;
             that.drawPieProgress(progress);
+            that.progress.text = progress + '%';
         }
     };
 
